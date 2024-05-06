36a78515516147ab4993e6742a234270b0829fdf
Zawent <fabianvargas1099@gmail.com>
2024-05-06 14:16:47 -0500

    intento de arrelglar proyecto v1

---
+++ src/app/empresario/add-empresa/add-empresa.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-empresa',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-empresa.component.html',
+  styleUrl: './add-empresa.component.css'
+})
+export class AddEmpresaComponent {
+
+}
---
+++ src/app/empresario/list-empresas/list-empresas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-empresas',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-empresas.component.html',
+  styleUrl: './list-empresas.component.css'
+})
+export class ListEmpresasComponent {
+
+}
