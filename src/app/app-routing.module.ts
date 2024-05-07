65ae56c8ee956e5dbae2180b37ac9d86794fac05
Seedlab2024 <seedlabtecnoparque@gmail.com>
2024-05-07 14:26:59 -0500

    correcion error mergue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-=======
+  { path: 'empresario', component: AddEmpresaComponent }
+];
+
 import { AddEmpresaComponent } from './empresario/add-empresa/add-empresa.component';
 
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'empresario', component: AddEmpresaComponent }
->>>>>>> 20def69b0857d40d75feaa338e53bde9896b728a
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
