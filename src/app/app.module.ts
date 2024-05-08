809ccfc50580465446ecf00265c8dd8ab83e2905
Zawent <fabianvargas1099@gmail.com>
2024-05-08 10:35:05 -0500

    Terminado add empresa y creacion de encuesta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { MenuComponent } from './inicio/menu/menu.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncuestaEmpresaComponent } from './empresario/encuesta-empresa/encuesta-empresa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    EncuestaEmpresaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/empresario/encuesta-empresa/encuesta-empresa.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encuesta-empresa',
+  templateUrl: './encuesta-empresa.component.html',
+  styleUrl: './encuesta-empresa.component.css'
+})
+export class EncuestaEmpresaComponent {
+
+}
