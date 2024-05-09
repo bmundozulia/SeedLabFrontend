fecc60a1403299d94de3c99c036444be0632c6b5
EstSant <h.andreaortiz1325@gmail.com>
2024-05-09 11:02:59 -0500

    correcion errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,11 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { HeaderComponent } from './header/header.component';
 import { EncuestaEmpresaComponent } from './empresario/encuesta-empresa/encuesta-empresa.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
-import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
 import { CommonModule } from '@angular/common';
 import { AddEmpresaComponent } from './empresario/add-empresa/add-empresa.component';
 import { NavegacionComponent } from './navegacion/navegacion.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
