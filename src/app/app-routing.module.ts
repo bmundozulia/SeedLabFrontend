82c04b7d08a5611b1a11f67af32c6e63acf9e035
JAAP2104 <almeidaperezjosephalexis@gmail.com>
2024-05-09 10:37:27 -0500

    se implemento nuevamente la barra lateral con angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
-import { NavegacionComponent } from './navegacion/navegacion.component'
+import { AddEmpresaComponent } from './empresario/add-empresa/add-empresa.component';
 import { VerificationComponent } from './verification/verification.component';
 
 
@@ -11,7 +10,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'empresario', component: AddEmpresaComponent },
-  { path: 'navegacion', component: NavegacionComponent },
+
   { path: 'verification', component: VerificationComponent }
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,12 @@ import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
 import { CommonModule } from '@angular/common';
 import { AddEmpresaComponent } from './empresario/add-empresa/add-empresa.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 
@@ -23,12 +29,18 @@ import { AddEmpresaComponent } from './empresario/add-empresa/add-empresa.compon
     HeaderComponent,
     EncuestaEmpresaComponent,
     AddEmpresaComponent,
+    NavegacionComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [
     provideClientHydration(),
