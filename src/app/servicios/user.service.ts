3e343fde15cae5d16923a8d165aa3c18562e6ca7
EstSant <h.andreaortiz1325@gmail.com>
2024-05-10 16:38:37 -0500

    url de empresas y servicios y modelos

---
+++ src/app/servicios/user.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  url='http://localhost:8000/api/auth'
+
+  constructor() { }
+}
