10d4fd3dcffebfedccb507d7e0c6e15a207ed1be
JAAP2104 <almeidaperezjosephalexis@gmail.com>
2024-05-15 17:38:23 -0500

    menu lateral quedo listo implementando css y tailwind css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,18 @@ import { RegisterComponent } from './register/register.component';
 import { AddEmpresaComponent } from './empresario/add-empresa/add-empresa.component';
 import { VerificationComponent } from './verification/verification.component';
 import { BodyComponent } from './inicio/body/body.component';
+import { MenuComponent } from './inicio/menu/menu.component'
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'menu', component: MenuComponent },
   { path: 'empresario', component: AddEmpresaComponent },
   { path: 'verification', component: VerificationComponent },
-  { path: 'login', component: LoginComponent }, 
-  { path: '', component: BodyComponent }, 
-  { path: '**', redirectTo: '' } 
+  { path: 'login', component: LoginComponent },
+  { path: '', component: BodyComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
