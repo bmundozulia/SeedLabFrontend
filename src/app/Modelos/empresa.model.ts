23d768991f044b175f450707d009db673774c5e8
Darienpes29 <145672787+Y3sith@users.noreply.github.com>
2024-05-22 11:06:46 -0500

    intento arreglo municipio

--- src/app/Modelos/empresa.model.ts
+++ src/app/Modelos/empresa.model.ts
@@ -3,7 +3,7 @@ import { Emprendedor } from "./emprendedor.model";
 export class Empresa {
 
     documento?: number;
-    nombre: string | null | undefined;
+    nombre?: string | null | undefined;
     cargo: string | null | undefined;
     razonSocial: string | null | undefined;
     url_pagina: string | null | undefined
@@ -14,26 +14,26 @@ export class Empresa {
     correo: string | null | undefined;
     experiencia: string | null | undefined;
     funciones: string | null | undefined;
-    id_tipo_documento:number;
-    id_municipio:number;
-    id_emprendedor: number;
+    id_tipo_documento?:number;
+    id_municipio?:number;
+    id_emprendedor?: number;
 
     constructor(
-        documento: number,
-        nombre: string | null | undefined,
-        cargo: string | null | undefined,
-        razonSocial: string | null | undefined,
-        url_pagina: string | null | undefined,
-        telefono: number,
-        celular: number,
-        direccion: string | null | undefined,
-        profesion: string | null | undefined,
-        correo: string | null | undefined,
-        experiencia: string | null | undefined,
-        funciones: string | null | undefined,
-        id_tipo_documento:number,
-        id_municipio:number,
-        id_emprendedor: number){
+        documento?: number,
+        nombre?: string | null | undefined,
+        cargo?: string | null | undefined,
+        razonSocial?: string | null | undefined,
+        url_pagina?: string | null | undefined,
+        telefono?: number,
+        celular?: number,
+        direccion?: string | null | undefined,
+        profesion?: string | null | undefined,
+        correo?: string | null | undefined,
+        experiencia?: string | null | undefined,
+        funciones?: string | null | undefined,
+        id_tipo_documento?:number,
+        id_municipio?:number,
+        id_emprendedor?: number){
             this.documento = documento;
             this.nombre = nombre;
             this.cargo = cargo;
