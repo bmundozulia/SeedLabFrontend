c646727152030ac54a65dd47e6fe86ca928bee7d
Marly <rangelmarly@gmail.com>
2024-05-22 09:13:22 -0500

    personalizaciones

--- src/app/personalizaciones/personalizaciones.component.ts
+++ src/app/personalizaciones/personalizaciones.component.ts
@@ -1,46 +1,44 @@
 import { Component, QueryList, ViewChildren, ElementRef } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-personalizaciones',
   templateUrl: './personalizaciones.component.html',
-  styleUrls: ['./personalizaciones.component.css']
+  styleUrls: ['./personalizaciones.component.css'], // Cambiado a plural
 })
 export class PersonalizacionesComponent {
   selectedColorPrincipal = '#C2FFFB';
   selectedColorSecundario = '#C2FFFB';
   previewUrl: any = null;
-
+  
 
   @ViewChildren('colorPicker') colorPickers: QueryList<ElementRef>;
 
-  openColorPicker(index: number) {
+  openColorPicker(index: number): void {
     const colorPicker = this.colorPickers.toArray()[index];
     if (colorPicker) {
       colorPicker.nativeElement.click();
     }
   }
 
-  onColorChangePrincipal(event) {
-    this.selectedColorPrincipal = event.target.value;
+  onColorChangePrincipal(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.selectedColorPrincipal = input.value;
   }
 
-  onColorChangeSecundario(event) {
-    this.selectedColorSecundario = event.target.value;
+  onColorChangeSecundario(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.selectedColorSecundario = input.value;
   }
-  onFileChange(event) {
-    let reader = new FileReader();
-    if(event.target.files && event.target.files.length) {
-      const [file] = event.target.files;
+
+  onFileChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length) {
+      const file = input.files[0];
+      const reader = new FileReader();
       reader.readAsDataURL(file);
-  
       reader.onload = () => {
         this.previewUrl = reader.result;
-      }
+      };
     }
   }
-  
 }
-
-
