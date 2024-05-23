8753b278fce8da4badd0ec443ff6a6a6aece9147
Zawent <fabianvargas1099@gmail.com>
2024-05-23 15:13:47 -0500

    Ya lista, y crea asesorias por parte del emprendedor

--- src/app/servicios/asesoria.service.ts
+++ src/app/servicios/asesoria.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environment/env';
 
@@ -8,18 +7,37 @@ import { environment } from '../../environment/env';
   providedIn: 'root'
 })
 export class AsesoriaService {
-
-  private apiUrl = `${environment.apiUrl}mis_asesorias`;
+  private apiUrl = `${environment.apiUrl}asesorias/`;
 
   constructor(private http: HttpClient) { }
 
   getMisAsesorias(body: { documento: string, asignacion: boolean }): Observable<any> {
+    const token = localStorage.getItem('token'); // Obtén el token del localStorage
+    if (!token) {
+      console.error('Token no encontrado en el localStorage');
+      return new Observable<any>();
+    }
+
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer tu-token-aqui'  // Asegúrate de reemplazar esto con tu token real
+      'Authorization': `Bearer ${token}`  // Incluye el token en las cabeceras
     });
-    return this.http.post<any>(this.apiUrl, body, { headers });
+
+    return this.http.post<any>(`${this.apiUrl}mis_asesorias`, body, { headers });
   }
 
-  
+  crearAsesoria(data: any): Observable<any> {
+    const token = localStorage.getItem('token'); // Obtén el token del localStorage
+    if (!token) {
+      console.error('Token no encontrado en el localStorage');
+      return new Observable<any>();
+    }
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`  // Incluye el token en las cabeceras
+    });
+
+    return this.http.post<any>(`${this.apiUrl}solicitud_asesoria`, data, { headers });
+  }
 }
