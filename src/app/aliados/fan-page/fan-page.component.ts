ecad6252269589b5e2fcd5eede8978377e8ce8f2
deicyjejen21 <139778113+deicyjejen21@users.noreply.github.com>
2024-05-27 10:23:01 -0500

    vista fan-page

--- src/app/aliados/fan-page/fan-page.component.ts
+++ src/app/aliados/fan-page/fan-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { faFileLines} from '@fortawesome/free-solid-svg-icons';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faFileUpload } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-fan-page',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './fan-page.component.css'
 })
 export class FanPageComponent {
+  imagePreview: string | ArrayBuffer | null = null;
+  faFileLines = faFileLines;
+  hide = true;
+  showPassword = faEye;
+  faFileUpload=faFileUpload;
 
+onFileSelected(event: Event): void {
+  const input = event.target as HTMLInputElement;
+  if (input.files && input.files[0]) {
+    const file = input.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+}
 }
