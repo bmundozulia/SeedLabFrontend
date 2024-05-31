5b0bfec604d8775c048f38de047d2e2d1000e3bb
JAAP2104 <almeidaperezjosephalexis@gmail.com>
2024-05-31 15:56:08 -0500

    se hizo validacion de los campos del  modal crear orientador

--- src/app/orientador/orientador-crear/modal-crear-orientador/modal-crear-orientador.component.ts
+++ src/app/orientador/orientador-crear/modal-crear-orientador/modal-crear-orientador.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { SwitchService } from '../../../servicios/switch.service';
 
 @Component({
   selector: 'app-modal-crear-orientador',
   templateUrl: './modal-crear-orientador.component.html',
-  styleUrl: './modal-crear-orientador.component.css'
+  styleUrls: ['./modal-crear-orientador.component.css']
 })
 export class ModalCrearOrientadorComponent implements OnInit {
   @Input() isEditing: boolean = false;
-  constructor(private modalCRO: SwitchService) {
+  submitted: boolean = false;
 
-  }
-  ngOnInit(): void {
+  constructor(private modalCRO: SwitchService) { }
 
-  }
+  ngOnInit(): void { }
 
   cancelarCrearOrientador() {
     this.modalCRO.$modalCrearOrientador.emit(false);
   }
-  guardarCrearOrientador() {
-    this.modalCRO.$modalCrearOrientador.emit(false);
+
+  confirmarCrearOrientador(formulario: NgForm) {
+    this.submitted = true;
+
+    if (formulario.valid) {
+      this.modalCRO.$modalCrearOrientador.emit(false);
+    }
   }
 }
