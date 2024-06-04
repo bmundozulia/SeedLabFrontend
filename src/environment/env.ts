b3d1dd3fbc2d6dc6715937c8f6ce63c431f0efb2
deicyjejen21 <139778113+deicyjejen21@users.noreply.github.com>
2024-06-04 09:14:39 -0500

    personalizaciones

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -3,5 +3,7 @@ export const environment = {
   // apiUrl: 'http://10.90.51.174:8000/api/'
   //apiUrl: 'http://10.90.50.159:8000/api/'
   //apiUrl: 'http://127.0.0.1:8000/api/'
-  apiUrl: 'http://10.90.50.159:8000/api/'
+  //apiUrl: 'http://10.90.50.159:8000/api/'
+  apiUrl: 'http://10.90.51.137:8000/api/'
+
 };
