1e77b1aad26eb6ef48484470635fb6cbf9f42be3
deicyjejen21 <139778113+deicyjejen21@users.noreply.github.com>
2024-07-02 16:48:26 -0500

    home

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
   
