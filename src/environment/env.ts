94834e72a5cfe01a66d60e7abccdbdcc5ce89d02
BrayanEFJ <brayanfigueroajerez@gmail.com>
2024-07-17 16:15:59 -0500

    actualizacion de logica de negocios login
    se actualizo el inicio de sesion con alertas incluidas tanto del back como del front, falta probar mas a detalle para evaluar

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://10.90.51.73:8000/api/'
-
+  //  apiUrl: 'http://10.90.51.73:8000/api/'
   //apiUrl: 'http://10.90.50.97:8000/api/'
-  //apiUrl: 'http://10.90.50.159:8000/api/'
-  //apiUrl: 'http://127.0.0.1:8000/api/'
+  apiUrl: 'http://127.0.0.1:8000/api/'
+  // apiUrl: 'http://10.90.50.159:8000/api/'
+  //apiUrl: 'http://127.0.0.1:4545/api/'
   //apiUrl: 'https://seedlab-back-1.onrender.com/api/'
 };
