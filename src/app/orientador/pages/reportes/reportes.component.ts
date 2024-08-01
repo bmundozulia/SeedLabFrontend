fc0e03c123686ec13a85316d153fc50231800a9e
Esteban J. Avila <estebanavila2704@gmail.com>
2024-08-01 08:53:59 -0500

    Validates completos

--- src/app/orientador/pages/reportes/reportes.component.ts
+++ src/app/orientador/pages/reportes/reportes.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { faPrint } from '@fortawesome/free-solid-svg-icons';
+import { Router } from '@angular/router';
+import { User } from '../../../Modelos/user.model';
 
 
 
@@ -11,4 +13,36 @@ import { faPrint } from '@fortawesome/free-solid-svg-icons';
 export class ReportesComponent {
   faPrint = faPrint;
 
+  token = '';
+  user: User | null = null;
+  id: number | null = null;
+  currentRolId: number;
+
+  constructor(private router: Router){
+  
+  }
+
+  ngOnInit(): void {
+    this.validateToken();    
+  }
+
+  validateToken(): void {
+    if(!this.token) {
+      this.token = localStorage.getItem('token');
+      let identityJSON = localStorage.getItem('identity');
+      
+      if(identityJSON) {
+        let identity = JSON.parse(identityJSON);
+        this.user = identity;
+        this.id = this.user.id;
+        this.currentRolId = this.user.id_rol;
+        if (this.currentRolId != 4) {
+          this.router.navigate(['home']);
+        }
+      }
+    }
+    if (!this.token) {
+      this.router.navigate(['home']);
+    }
+  }
 }
