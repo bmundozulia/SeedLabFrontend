7a332a68c341b861051a9d12ffd3f5e5b5e3e30e
bebelinn <145699248+bebelinn@users.noreply.github.com>
2024-08-02 16:56:10 -0500

    ya se aplico echarts a dos graficas funcional, falta una

--- src/app/superadmin/pages/dashboard/dashboard.component.ts
+++ src/app/superadmin/pages/dashboard/dashboard.component.ts
@@ -1,102 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { User } from '../../../Modelos/user.model';
 import { SuperadminService } from '../../../servicios/superadmin.service';
-import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
 import { AliadoService } from '../../../servicios/aliado.service';
 import { Router } from '@angular/router';
-
+import * as echarts from 'echarts';
+import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
+export class DashboardComponent implements AfterViewInit {
+  token: string | null = null;
+  user: User = null;
+  id: number;
+  currentRolId: number = null;
+  totalUsuarios: any[] = [];
+  totalSuperAdmin: any = {};
+  totalOrientador: any = {};
+  totalAliados: any = {};
+  totalAsesores: any = {};
+  totalEmprendedores: any = {};
+  topAliados: any = {};
+  pieChartOption: echarts.EChartsOption;
+  doughnutChartOption: echarts.EChartsOption;
 
-export class DashboardComponent {
+  // Barras top_aliados
+  public barChartOptions: ChartOptions<'bar'> = {
+    responsive: true,
+    maintainAspectRatio: false 
+  };
 
-token: string | null = null;
-user: User = null;
-id:number;
-currentRolId:number = null;
-totalUsuarios:any[] = [];
-totalSuperAdmin: any = {};
-totalOrientador:any = {};
-totalAliados:any = {};
-totalAsesores:any = {};
-totalEmprendedores:any = {};
-topAliados:any = {};
+  public barChartType: ChartType = 'bar';
+  topAliadosLabels: string[] = [];
+  public topAliadosData: ChartDataset[] = [
+    {
+      label: 'Top Aliados',
+      data: [0, 0],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+      ],
+    }
+  ];
 
-//Barras top_aliados
-public barChartOptions: ChartOptions<'bar'> ={
-  responsive:true,
-  maintainAspectRatio:false 
-};
+  // Pie-Asesorias
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+  public pieChartLabels: string[] = ['Asesorias asignadas', 'Asesorias sin asignar'];
+  public pieChartData: ChartDataset<'pie'>[] = [];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
 
-public barChartType: ChartType = 'bar';
+  // Gráfica géneros
+  public doughnutChartOptions: ChartOptions<'doughnut'> = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+  public doughnutChartLabels: string[] = ['Femenino', 'Masculino', 'Otros'];
+  public doughnutChartData: ChartDataset<'doughnut'>[] = [];
 
-topAliadosLabels: string[] = [];
-public topAliadosData:ChartDataset[] = [
-  {
-    label: 'Top Aliados',
-    data: [0,0],
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(255, 206, 86, 0.2)',
-    ],
+  constructor(
+    private superAdminService: SuperadminService,
+    private aliadoService: AliadoService,
+    private router: Router,
+  ) {}
+
+  ngOnInit() {
+    this.validateToken();
+    this.getDatosDashboard();
+    this.getDatosGenerosGrafica();
   }
-];
 
-//Pie-Asesorias
-public pieChartOptions: ChartOptions<'pie'> = {
-  responsive: true,
-  maintainAspectRatio:false
-}
-public pieChartLabels: string[] =['Asesorias asignadas', 'Asesorias sin asignar'];
+  ngAfterViewInit() {
+    // El gráfico se inicializará cuando se carguen los datos
+  }
 
-public pieChartData: ChartDataset<'pie'>[] = [];
-public pieChartLegend = true;
-public pieChartPlugins = [];
+  validateToken(): void {
+    if (!this.token) {
+      this.token = localStorage.getItem('token');
+      let identityJSON = localStorage.getItem('identity');
 
-//Grafica generos
-public doughnutChartOptions: ChartOptions<'doughnut'> = {
-  responsive:true,
-  maintainAspectRatio:false
-}
-
-public doughnutChartLabels: string[] = ['Femenino', 'Masculino', 'Otros'];
-
-public doughnutChartData: ChartDataset<'doughnut'>[] = [];
-
-
-
-constructor(
-  private superAdminService:SuperadminService,
-  private aliadoService:AliadoService,
-  private router: Router,
-
-) {}
-
-ngOnInit() {
-  this.validateToken();
-  this.getDatosDashboard();
-  this.getDatosGenerosGrafica();
-};
-
-
-validateToken():void{
-  if(!this.token){
-    this.token = localStorage.getItem('token');
-    let identityJSON = localStorage.getItem('identity');
-
-    if(identityJSON){
-      let identity = JSON.parse(identityJSON);
-      this.user = identity;
-      this.id = this.user.id;
-      this.currentRolId = this.user.id_rol;
-      console.log(this.currentRolId);
-      if(this.currentRolId != 1){
-        this.router.navigate(['home']);
+      if (identityJSON) {
+        let identity = JSON.parse(identityJSON);
+        this.user = identity;
+        this.id = this.user.id;
+        this.currentRolId = this.user.id_rol;
+        console.log(this.currentRolId);
+        if (this.currentRolId != 1) {
+          this.router.navigate(['home']);
         }
       }
     }
@@ -105,60 +101,135 @@ validateToken():void{
     }
   }
 
-getDatosDashboard():void{
-  this.superAdminService.dashboardAdmin(this.token).subscribe(
-    data => {
-      this.totalUsuarios = data;
-      this.totalSuperAdmin = data.superadmin;
-      this.totalOrientador = data.orientador;
-      this.totalAliados = data.aliado;
-      this.totalAsesores = data.asesor;
-      this.totalEmprendedores = data.emprendedor;
-      this.topAliados = data.topAliados;
-      this.topAliadosLabels = this.topAliados.map(aliado => aliado.nombre);
-      this.topAliadosData[0].data = this.topAliados.map(aliado => aliado.asesorias);
-      this.pieChartData = [{
-        label: 'Asesorias',
-        data: [data.conteoAsesorias.asesoriasAsignadas, data.conteoAsesorias.asesoriasSinAsignar],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)'
-        ],
-      }];
-      console.log(data);
-    },
-    error => {
-      console.log(error);
+  getDatosDashboard(): void {
+    this.superAdminService.dashboardAdmin(this.token).subscribe(
+      data => {
+        this.totalUsuarios = data;
+        this.totalSuperAdmin = data.superadmin;
+        this.totalOrientador = data.orientador;
+        this.totalAliados = data.aliado;
+        this.totalAsesores = data.asesor;
+        this.totalEmprendedores = data.emprendedor;
+        this.topAliados = data.topAliados;
+        this.topAliadosLabels = this.topAliados.map(aliado => aliado.nombre);
+        this.topAliadosData[0].data = this.topAliados.map(aliado => aliado.asesorias);
+        this.pieChartOption = {
+          tooltip: {
+            trigger: 'item'
+          },
+          legend: {
+            top: '5%',
+            left: 'center'
+          },
+          series: [
+            {
+              name: 'Asesorías',
+              type: 'pie',
+              radius: ['40%', '70%'],
+              avoidLabelOverlap: false,
+              itemStyle: {
+                borderRadius: 10
+              },
+              label: {
+                show: false,
+                position: 'center'
+              },
+              emphasis: {
+                label: {
+                  show: true,
+                  fontSize: 40,
+                  fontWeight: 'bold'
+                }
+              },
+              labelLine: {
+                show: false
+              },
+              data: [
+                { value: data.conteoAsesorias.asesoriasAsignadas, name: 'Asesorías asignadas' },
+                { value: data.conteoAsesorias.asesoriasSinAsignar, name: 'Asesorías sin asignar' }
+              ]
+            }
+          ]
+        };
+
+        // Inicializa el gráfico aquí después de obtener los datos
+        this.initEChartsPie();
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  initEChartsPie(): void {
+    const chartDom = document.getElementById('echarts-pie');
+    if (chartDom) {
+      const myChart = echarts.init(chartDom);
+      myChart.setOption(this.pieChartOption);
+    } else {
+      console.error('No se pudo encontrar el elemento con id "echarts-pie"');
     }
-  )
-}
+  }
 
-getDatosGenerosGrafica():void{
-  this.aliadoService.graficaDatosGeneros(this.token).subscribe(
-    data => {
-      console.log('data generos',data);
-      const dataGenero = data.map(item => item.total);
-      this.doughnutChartData =[{
-        label: 'Generos',
-        data: dataGenero,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)'
-        ],
-        hoverBackgroundColor: [
-          'rgba(255, 99, 132, 0.3)',
-          'rgba(54, 162, 235, 0.3)',
-          'rgba(255, 206, 86, 0.3)'
-        ]
-      }]
-    },
-    error => {
-      console.log(error);
+  getDatosGenerosGrafica(): void {
+    this.aliadoService.graficaDatosGeneros(this.token).subscribe(
+      data => {
+        console.log('data generos', data);
+        const dataGenero = data.map(item => item.total);
+        this.doughnutChartOption = {
+          tooltip: {
+            trigger: 'item'
+          },
+          legend: {
+            top: '5%',
+            left: 'center'
+          },
+          series: [
+            {
+              name: 'Géneros',
+              type: 'pie',
+              radius: ['40%', '70%'],
+              avoidLabelOverlap: false,
+              itemStyle: {
+                borderRadius: 10
+              },
+              label: {
+                show: false,
+                position: 'center'
+              },
+              emphasis: {
+                label: {
+                  show: true,
+                  fontSize: 40,
+                  fontWeight: 'bold'
+                }
+              },
+              labelLine: {
+                show: false
+              },
+              data: data.map(item => ({ value: item.total, name: item.genero }))
+            }
+          ]
+        };
+
+        // Inicializa el gráfico aquí después de obtener los datos
+        this.initEChartsDoughnut();
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  initEChartsDoughnut(): void {
+    const chartDom = document.getElementById('echarts-doughnut');
+    if (chartDom) {
+      const myChart = echarts.init(chartDom);
+      myChart.setOption(this.doughnutChartOption);
+    } else {
+      console.error('No se pudo encontrar el elemento con id "echarts-doughnut"');
     }
-  )
-}
-
-
-
+  }
 }
