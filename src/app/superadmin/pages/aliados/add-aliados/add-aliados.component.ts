533ce527124508879c10aa586cbc7920a3b04be3
bebelinn <145699248+bebelinn@users.noreply.github.com>
2024-08-09 12:07:25 -0500

    ajustando add aliados

--- src/app/superadmin/pages/aliados/add-aliados/add-aliados.component.ts
+++ src/app/superadmin/pages/aliados/add-aliados/add-aliados.component.ts
@@ -47,11 +47,16 @@ export class AddAliadosComponent {
   faEyeSlash = faEyeSlash;
   faFileUpload = faFileUpload;
   faFileLines = faFileLines;
-  imagePreview: string | ArrayBuffer | null = null;
   aliadoid: string;
-
   bannerForm: FormGroup;
   aliadoForm: FormGroup;
+  showFirstSection = true;
+  showSecondSection = false;
+  showThirdSection = false;
+  imagePreviewLogo: string | ArrayBuffer | null = null;  
+  imagePreviewBanner: string | ArrayBuffer | null = null;
+  imagePreview: string | ArrayBuffer | null = null;
+
 
   constructor(private aliadoService: AliadoService,
     private actividadService: ActividadService,
@@ -251,31 +256,40 @@ export class AddAliadosComponent {
       if (field === 'urlImagen') {
         this.selectedBanner = file;
         this.bannerForm.patchValue({ urlImagen: file });
+        this.generateImagePreview(file, 'banner');
       }
       if (field === 'logo') {
         this.selectedLogo = file;
         this.aliadoForm.patchValue({ logo: file });
+        this.generateImagePreview(file, 'logo');
       }
-      if (field === 'ruta_multi') {
+      if (field === 'Imagen') { // Cambiado para coincidir con 'Imagen'
         this.selectedruta = file;
-        this.aliadoForm.patchValue({ ruta_multi: file });
+        this.aliadoForm.patchValue({ Imagen: file });
+        this.generateImagePreview(file, 'image'); // Cambiado a 'image'
       }
     } else {
-      // Si no se seleccionó un archivo, se puede asignar un valor de texto al campo ruta_multi
       if (field === 'ruta_multi') {
         this.aliadoForm.patchValue({ ruta_multi: event.target.value });
       }
     }
   }
 
-
-  generateImagePreview(file: File) {
+  generateImagePreview(file: File, type: string) {
     const reader = new FileReader();
     reader.onload = (e: any) => {
-      this.imagePreview = e.target.result;
+      if (type === 'logo') {
+        this.imagePreviewLogo = e.target.result;
+      } else if (type === 'banner') {
+        this.imagePreviewBanner = e.target.result;
+      } else if (type === 'image') { // Añadido para 'image'
+        this.imagePreview = e.target.result;
+      }
     };
     reader.readAsDataURL(file);
   }
+
+
 
   getFormValidationErrors(form: FormGroup) {
     const result: any = {};
@@ -302,5 +316,26 @@ export class AddAliadosComponent {
       img.src = URL.createObjectURL(file);
     });
   }
+
+  next() {
+    if (this.showFirstSection) {
+      this.showFirstSection = false;
+      this.showSecondSection = true;
+    } else if (this.showSecondSection) {
+      this.showSecondSection = false;
+      this.showThirdSection = true;
+    }
+  }
+
+  previous() {
+    if (this.showThirdSection) {
+      this.showThirdSection = false;
+      this.showSecondSection = true;
+    } else if (this.showSecondSection) {
+      this.showSecondSection = false;
+      this.showFirstSection = true;
+    }
+  }
+
 
 }
